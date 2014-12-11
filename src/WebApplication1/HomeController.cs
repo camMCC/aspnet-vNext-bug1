18bf29d85498543be38a7333b22432620bdac7be
Cam Birch <cam@maintenanceconnection.ca>
2014-12-11 11:09:38 -0700

    Basic minimal repro files.

---
+++ src/WebApplication1/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.Mvc;
+using System;
+
+namespace WebApplication1
+{
+
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index() {
+            return View();
+        }
+
+    }
+
+}
---
+++ src/WebApplication1/Startup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Http;
+using Microsoft.Framework.DependencyInjection;
+using Microsoft.AspNet.Routing;
+
+namespace WebApplication1
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+
+            app.UseServices(services => {
+                services.AddMvc();
+            });
+
+            app.UseMvc(routes => {
+                routes.MapRoute(
+                    name: "app",
+                    template: "{action}/{id?}",
+                    defaults: new { controller = "Home", action = "Index" });
+            });
+        }
+    }
+}
